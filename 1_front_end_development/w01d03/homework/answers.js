47334aec6101a1d913584580f5e30bb63164e0be
Senthil <spk2dc@virginia.edu>
2020-05-28 18:10:17 -0400

    Fizz Buzz answered

--- 1_front_end_development/w01d03/homework/answers.js
+++ 1_front_end_development/w01d03/homework/answers.js
@@ -9,3 +9,15 @@ for (let index = 1; index <= 20; index++) {
 for (let index = 0; index <= 200; index = index + 2) {
     console.log(index);
 }
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+        console.log("fizzbuzz");
+    } else if (index % 3 == 0) {
+        console.log("fizz");
+    } else if (index % 5 == 0) {
+        console.log("buzz");
+    } else{
+        console.log(index);
+    }
+}
