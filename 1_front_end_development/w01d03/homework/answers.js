536657fb024bdc1728fb7b9882b6c07710824c5c
Senthil <spk2dc@virginia.edu>
2020-05-29 00:21:51 -0400

    Find the Median answered

--- 1_front_end_development/w01d03/homework/answers.js
+++ 1_front_end_development/w01d03/homework/answers.js
@@ -96,3 +96,6 @@ for (let index = 0; index < 20; index++) {
 ////////////////////////////////
 //  Find the Median
 ////////////////////////////////
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+nums.sort();
+console.log(nums[Math.floor(nums.length/2)]);
