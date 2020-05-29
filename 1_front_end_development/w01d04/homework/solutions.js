15660df6b5292be56f06aed5484416e92b344da5
Senthil <spk2dc@virginia.edu>
2020-05-29 18:03:01 -0400

    verbal questions completed

---
+++ 1_front_end_development/w01d04/homework/solutions.js
@@ -0,0 +1,6 @@
+/*
+VERBAL QUESTIONS
+1. An argument is the value you pass into a method call. A parameter is the value passed into the method as an input. 
+2. console log prints to the terminal, but return sends a value back to whatever called the function.
+
+*/
