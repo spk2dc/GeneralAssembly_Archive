47ee4b9c1b9dfb445a6f08ee0da970a0db5cf233
Senthil <spk2dc@virginia.edu>
2020-06-01 22:07:04 -0400

    w2d1 hw

---
+++ 1_front_end_development/w02d01/student_labs/pseudocode.js
@@ -0,0 +1,18 @@
+let start = prompt("Do you want waffles?")
+if (start == "yes") {
+    console.log("Good, let's begin making them.");
+} else{
+    console.log("Too bad, you're going to get them anyways.");
+}
+
+console.log("Go to fridge");
+console.log("Open freezer");
+console.log("If there are waffles then take them out");
+console.log("Toast waffles until brown");
+console.log("Get a plate");
+console.log("Put waffles on plate");
+console.log("Pour syrup until it covers at least half waffle");
+console.log("Get strawberries and cut them");
+console.log("Place diced strawberries on top of waffle");
+console.log("Bring plate to table and get silverware");
+console.log("Enjoy waffles.");
