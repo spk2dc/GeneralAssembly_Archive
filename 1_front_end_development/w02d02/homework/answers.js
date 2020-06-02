c376650da541c8c95a942db47622aaf8ebb93b06
Senthil <spk2dc@virginia.edu>
2020-06-02 17:39:37 -0400

    datatype refresher complete

---
+++ 1_front_end_development/w02d02/homework/answers.js
@@ -0,0 +1,28 @@
+/*
+DATATYPES REFRESHER
+
+A light switch that can be either on or off.
+=> datatype: boolean 
+=> data structure example: const bool = true; 
+
+A user's email address.
+=> datatype: string 
+=> data structure example: let x = 'sam@gmail.com'; 
+
+A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+=> datatype: object 
+=> data structure example: let x = {hull, lasers, etc}; 
+
+A list of student names from our class.
+=> datatype: array 
+=> data structure example: let x = ['name1', 'name2']; 
+
+A list of student names from our class, each with a location.
+=> datatype: array of objects
+=> data structure example: let x = [{name, location},{name2, location2}]; 
+
+A list of student names from our class, each with a location and each with a list of favorite tv shows.
+=> datatype: array of objects 
+=> data structure example: let x = [{name, location, [show1, show2]},{name2, location2, [show1, show2]}]; 
+
+*/
