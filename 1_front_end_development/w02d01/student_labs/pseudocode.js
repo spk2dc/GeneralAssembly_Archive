c367258237fc3a218b26fb75c343746f3fa9c1f9
Senthil <spk2dc@virginia.edu>
2020-06-01 22:12:23 -0400

    w1d1 hw

--- 1_front_end_development/w02d01/student_labs/pseudocode.js
+++ 1_front_end_development/w02d01/student_labs/pseudocode.js
@@ -1,18 +1,18 @@
-let start = prompt("Do you want waffles?")
+let start = prompt("Do you want waffles?") //conditional
 if (start == "yes") {
-    console.log("Good, let's begin making them.");
+    console.log("Good, let's begin making them."); //assertion
 } else{
-    console.log("Too bad, you're going to get them anyways.");
+    console.log("Too bad, you're going to get them anyways."); //assertion
 }
 
-console.log("Go to fridge");
-console.log("Open freezer");
-console.log("If there are waffles then take them out");
-console.log("Toast waffles until brown");
-console.log("Get a plate");
-console.log("Put waffles on plate");
-console.log("Pour syrup until it covers at least half waffle");
-console.log("Get strawberries and cut them");
-console.log("Place diced strawberries on top of waffle");
-console.log("Bring plate to table and get silverware");
-console.log("Enjoy waffles.");
+console.log("Go to fridge"); //assertion
+console.log("Open freezer"); //assertion
+console.log("If there are waffles then take them out"); //conditional
+console.log("Toast waffles until brown"); //loop
+console.log("Get a plate"); //assertion
+console.log("Put waffles on plate"); //assertion
+console.log("Pour syrup until it covers at least half waffle"); //loop
+console.log("Get strawberries and cut them"); //assertion
+console.log("Place diced strawberries on top of waffle"); //assertion
+console.log("Bring plate to table and get silverware"); //assertion
+console.log("Enjoy waffles."); //conditional
