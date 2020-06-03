af0ed19b3d846d31c7b3310ee1f31370487f4eb0
Senthil <spk2dc@virginia.edu>
2020-06-03 00:38:54 -0400

    crazy object complete

--- 1_front_end_development/w02d02/homework/answers.js
+++ 1_front_end_development/w02d02/homework/answers.js
@@ -41,8 +41,43 @@ let senthil = {
 console.log(senthil.hobby);
 
 //CRAZY OBJECT
+const crazyObject = {
+    taco: [{
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+    ],
+    larry: {
+        nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+        quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+        characters: [{
+            name: "Jeff",
+            occupation: "manager"
+        },
+        {
+            name: "funkhauser",
+            occupation: "tv dude"
+        },
+        {
+            name: "susie",
+            occupation: "jeffs wife",
+            favourtieHobby: "Swearing at Larry and Jeff"
+        },
+        ]
+    }
+}
 
-
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1]);
+console.log(crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk"));
+console.log(crazyObject.larry.quotes[4]);
 
 
 //OBJECT-CEPTION
