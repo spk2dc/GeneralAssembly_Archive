73b7991a0456017acf60f17d47e97caa3fbd1db6
Senthil <spk2dc@virginia.edu>
2020-06-04 17:42:48 -0400

    created person class

--- 1_front_end_development/w02d04/homework/objects.js
+++ 1_front_end_development/w02d04/homework/objects.js
@@ -15,3 +15,48 @@ class Hamster {
         return this.price;
     }
 }
+
+
+class Person {
+    constructor(name) {
+        this.age = 0;
+        this.name = name;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log('hello ' + this.name);
+    }
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+    exercise() {
+        this.weight--;
+    }
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount += 10;
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
+    }
+}
