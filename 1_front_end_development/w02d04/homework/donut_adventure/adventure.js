b890e701dc96b8fb6ba8ec947d7927038be15bcd
Senthil <spk2dc@virginia.edu>
2020-06-05 11:25:16 -0400

    created our enemy pizza rat

--- 1_front_end_development/w02d04/homework/donut_adventure/adventure.js
+++ 1_front_end_development/w02d04/homework/donut_adventure/adventure.js
@@ -7,7 +7,7 @@ class Hero {
             sugarShock: 10
         };
         this.catchPhrases = ['i\'m fresher than day old pizza',
-            'you can\'t count my calories']
+            'you can\'t count my calories'];
     }
 
     talkSass() {
@@ -18,9 +18,34 @@ class Hero {
         console.log(this.health);
     }
 
-    battle(){
+    battle() {
         console.log('i\'m ready to rumble');
     }
 }
 
 let dougie = new Hero('Dougie Donut');
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.objects = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        };
+        this.catchPhrases = ['i\'m youtube famous',
+            'i\'m more dangerous than an uncovered sewer'];
+    }
+
+    talkSmack() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]);
+    }
+
+    announceHealth() {
+        console.log(this.health);
+    }
+
+    battle() {
+        console.log('i\'m gonna flatten you like a slice of pepperoni!');
+    }
+}
