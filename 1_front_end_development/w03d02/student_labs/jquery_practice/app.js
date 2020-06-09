b247ccc9855d3aba40f320128f039548bdec2071
Senthil <spk2dc@virginia.edu>
2020-06-09 18:23:54 -0400

    fixed lab jquery problems

--- 1_front_end_development/w03d02/student_labs/jquery_practice/app.js
+++ 1_front_end_development/w03d02/student_labs/jquery_practice/app.js
@@ -1,21 +1,30 @@
 $(() => {
     console.log('test');
-    
-    const $div1 = $('<div>').attr('id', 'top-container')
-    let $h1 = $('h1').text('Trois couleurs: rouge, blanc, et blue')
+
+    const $div1 = $('<div>')
+    $div1.attr('id', 'top-container')
+    let $h1 = $('<h1>').text('Trois couleurs: rouge, blanc, et blue')
     $div1.append($h1)
 
     for (let i = 0; i < 3; i++) {
         let $div2 = $('<div>').addClass('couleur')
+        $div2.addClass('sub' + i)
         $div1.append($div2)
     }
+
+    $('body').append($div1)
     
+
     $('.couleur').css('height', '200px')
     $('.couleur').css('width', '200px')
     $('.couleur').css('display', 'inline-block')
-    $('.couleur').css('background-color',
-        'black')
+    $('.sub0').css('background-color',
+        'firebrick')
+    $('.sub1').css('background-color',
+        'ivory')
+    $('.sub2').css('background-color',
+        'cornflowerblue')
 
-    $('body').append($div1)
+
 
 });
