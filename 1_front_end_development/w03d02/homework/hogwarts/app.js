2a20de683b150416dfba5ff4beeda08f9897501f
Senthil <spk2dc@virginia.edu>
2020-06-09 18:38:09 -0400

    Commit 1 - I added my first element and it was fun

--- 1_front_end_development/w03d02/homework/hogwarts/app.js
+++ 1_front_end_development/w03d02/homework/hogwarts/app.js
@@ -1,35 +1,16 @@
-if (typeof jQuery == 'undefined') {
-    console.log('oops! I still have to link my jQuery properly!');
-} else { console.log('I did it! I linked jQuery and this js file!') };
+// if (typeof jQuery == 'undefined') {
+//     console.log('oops! I still have to link my jQuery properly!');
+// } else { console.log('I did it! I linked jQuery and this js file!') };
 
 
 $(() => {
-    console.log('test');
-
-    const $div1 = $('<div>')
-    $div1.attr('id', 'top-container')
-    let $h1 = $('<h1>').text('Trois couleurs: rouge, blanc, et blue')
-    $div1.append($h1)
-
-    for (let i = 0; i < 3; i++) {
-        let $div2 = $('<div>').addClass('couleur')
-        $div2.addClass('sub' + i)
-        $div1.append($div2)
-    }
-
-    $('body').append($div1)
-
-
-    $('.couleur').css('height', '200px')
-    $('.couleur').css('width', '200px')
-    $('.couleur').css('display', 'inline-block')
-    $('.sub0').css('background-color',
-        'firebrick')
-    $('.sub1').css('background-color',
-        'ivory')
-    $('.sub2').css('background-color',
-        'cornflowerblue')
-
+    const $container = $('#container')
+    console.log($container);
+    
+    const $h1 = $('<h1>').text('Hogwarts')
+    console.log($h1);
+    $container.append($h1)
+    $('body').css('text-align','center')
 
 
 });
