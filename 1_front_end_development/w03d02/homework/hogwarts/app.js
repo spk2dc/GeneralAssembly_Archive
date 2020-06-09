d3af6f368fc36852cfa71f06126f0e887760fb37
Senthil <spk2dc@virginia.edu>
2020-06-09 18:31:14 -0400

    Commit 0 - All my files are linked and loaded

---
+++ 1_front_end_development/w03d02/homework/hogwarts/app.js
@@ -0,0 +1,35 @@
+if (typeof jQuery == 'undefined') {
+    console.log('oops! I still have to link my jQuery properly!');
+} else { console.log('I did it! I linked jQuery and this js file!') };
+
+
+$(() => {
+    console.log('test');
+
+    const $div1 = $('<div>')
+    $div1.attr('id', 'top-container')
+    let $h1 = $('<h1>').text('Trois couleurs: rouge, blanc, et blue')
+    $div1.append($h1)
+
+    for (let i = 0; i < 3; i++) {
+        let $div2 = $('<div>').addClass('couleur')
+        $div2.addClass('sub' + i)
+        $div1.append($div2)
+    }
+
+    $('body').append($div1)
+
+
+    $('.couleur').css('height', '200px')
+    $('.couleur').css('width', '200px')
+    $('.couleur').css('display', 'inline-block')
+    $('.sub0').css('background-color',
+        'firebrick')
+    $('.sub1').css('background-color',
+        'ivory')
+    $('.sub2').css('background-color',
+        'cornflowerblue')
+
+
+
+});
