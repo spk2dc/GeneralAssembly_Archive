67a4b6f14700a39a07abb0e140c2592fe668373a
Senthil <spk2dc@virginia.edu>
2020-06-09 14:05:47 -0400

    dom exercise

---
+++ 1_front_end_development/w03d02/student_labs/dom/app.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').innerText = 'Welcome to GA';
+
+let eggos = document.createElement('img')
+document.querySelector('.container').appendChild(eggos);
+eggos.setAttribute('src', 'https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg');
+eggos.style.width = '50%'
