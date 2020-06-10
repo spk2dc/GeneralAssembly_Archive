bc91ceb5773941b269494451e7b1f9b7de9b4cf0
Senthil <spk2dc@virginia.edu>
2020-06-09 20:55:40 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- 1_front_end_development/w03d02/homework/hogwarts/app.js
+++ 1_front_end_development/w03d02/homework/hogwarts/app.js
@@ -6,11 +6,21 @@
 $(() => {
     const $container = $('#container')
     console.log($container);
-    
+
     const $h1 = $('<h1>').text('Hogwarts')
     console.log($h1);
     $container.append($h1)
-    $('body').css('text-align','center')
+    let $body = $('body').css('text-align', 'center')
 
+    let $h2 = $('<h2>').text('Senthil')
+    let $h3 = $('<h3>').text('Slytherin')
+    let $h4 = $('<h4>').text('Snowfire')
+    $h4.addClass('owl')
+    let $wand = $('<h4>').text('wand')
 
+    $body.append($h2)
+    $body.append($h3)
+    $body.append($h4)
+    $body.append($wand)
+    $('.owl').css('font-family', 'fantasy')
 });
