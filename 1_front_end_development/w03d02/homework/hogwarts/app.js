cfe3d21430c87fb95fe7d93f9165e2eb1b1e543f
Senthil <spk2dc@virginia.edu>
2020-06-09 21:19:11 -0400

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- 1_front_end_development/w03d02/homework/hogwarts/app.js
+++ 1_front_end_development/w03d02/homework/hogwarts/app.js
@@ -12,15 +12,29 @@ $(() => {
     $container.append($h1)
     let $body = $('body').css('text-align', 'center')
 
+    //year2
     let $h2 = $('<h2>').text('Senthil')
     let $h3 = $('<h3>').text('Slytherin')
     let $h4 = $('<h4>').text('Snowfire')
     $h4.addClass('owl')
     let $wand = $('<h4>').text('wand')
 
-    $body.append($h2)
-    $body.append($h3)
-    $body.append($h4)
-    $body.append($wand)
+    $container.append($h2)
+    $container.append($h3)
+    $container.append($h4)
+    $container.append($wand)
     $('.owl').css('font-family', 'fantasy')
+
+    //year3
+    let $ul = $('<ul>').attr('storage', 'trunk')
+        
+    $ul.append($('<li>').text('butter beer'))
+    $ul.append($('<li>').text('invisibility cloak').addClass('secret'))
+    $ul.append($('<li>').text('magic map').addClass('secret'))
+    $ul.append($('<li>').text('time turner').addClass('secret'))
+    $ul.append($('<li>').text('leash').addClass('owl'))
+    $ul.append($('<li>').text('jelly beans'))
+    $container.append($ul)
+    $('.secret').attr('opacity','0.5')
+    $ul.attr('list-style-type','none')
 });
