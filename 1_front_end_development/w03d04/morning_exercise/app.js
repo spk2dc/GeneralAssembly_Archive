15ed57bba0cac7b8ad6dd7bc87cf7b0438069968
Senthil <spk2dc@virginia.edu>
2020-06-11 11:16:54 -0400

    morning exercise

---
+++ 1_front_end_development/w03d04/morning_exercise/app.js
@@ -0,0 +1,11 @@
+
+$(() => {
+    let $container = $('.container')
+    let $a = $('<div>').addClass('A')
+    let $b = $('<div>').addClass('B')
+    let $c = $('<div>').addClass('C')
+
+    $container.append($a)
+    $container.append($b)
+    $container.append($c)
+})
