d77233333ee32b83e9f06204e6d7c8ebfe49c821
Senthil <spk2dc@virginia.edu>
2020-06-12 10:54:29 -0400

    finished morning exercise

--- 1_front_end_development/w03d05/morning_exercise/modal_practice/app.js
+++ 1_front_end_development/w03d05/morning_exercise/modal_practice/app.js
@@ -2,5 +2,33 @@
 // console.log('modal practice app.js is linked to this index.html');
 
 $( () => {
-  //write your code here
-});
+
+  //Grabbing Elements
+  const $openBtn = $('#openModal');
+  const $modal = $('#modal');
+  const $closeBtn = $('#close');
+  
+  //Event Handlers
+  const openModal = () => {
+    $modal.css('display', 'block');
+  }
+  
+  const closeModal = () => {
+    $modal.css('display', 'none');
+  }
+
+  const stopBubble = (event) => {
+    event.stopPropagation();
+  }
+  
+  //Event Listeners
+  $openBtn.on('click', openModal);
+  
+  $closeBtn.on('click', closeModal);
+
+  $('#modal').on('click', closeModal)
+  $('#modal-textbox').on('click', stopBubble)
+  
+  setTimeout(openModal, 5000);
+
+  }); //
