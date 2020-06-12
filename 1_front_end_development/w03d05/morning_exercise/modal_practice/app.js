5b1c0c4f5fb704b1dbe412a5b562337469c08d3c
Senthil <spk2dc@virginia.edu>
2020-06-12 12:18:19 -0400

    pallete picker

--- 1_front_end_development/w03d05/morning_exercise/modal_practice/app.js
+++ 1_front_end_development/w03d05/morning_exercise/modal_practice/app.js
@@ -1,34 +1,40 @@
 // console.log($);
 // console.log('modal practice app.js is linked to this index.html');
 
-$( () => {
+$(() => {
 
   //Grabbing Elements
   const $openBtn = $('#openModal');
   const $modal = $('#modal');
   const $closeBtn = $('#close');
-  
+  let modalOpened = false;
+
   //Event Handlers
   const openModal = () => {
-    $modal.css('display', 'block');
+    $modal.css('display', 'block').fadeIn(1000);
+    return true;
   }
-  
+
   const closeModal = () => {
-    $modal.css('display', 'none');
+    $modal.css('display', 'none').fadeOut(1000);
   }
 
   const stopBubble = (event) => {
     event.stopPropagation();
   }
-  
+
   //Event Listeners
   $openBtn.on('click', openModal);
+  console.log(modalOpened);
   
   $closeBtn.on('click', closeModal);
 
   $('#modal').on('click', closeModal)
   $('#modal-textbox').on('click', stopBubble)
-  
-  setTimeout(openModal, 5000);
 
-  }); //
+  if (!modalOpened) {
+    setTimeout(openModal, 5000);
+  }
+
+
+});
