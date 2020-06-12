c0a5b1387ba0fb52284d3a10dbfc5fe51c1ecf4f
Senthil <spk2dc@virginia.edu>
2020-06-12 16:15:06 -0400

    finished sketchpad lab

--- 1_front_end_development/w03d05/student_examples/sketchpad/sketchpad.js
+++ 1_front_end_development/w03d05/student_examples/sketchpad/sketchpad.js
@@ -1,2 +1,40 @@
-$(()=>{
+
+$(() => {
+
+    const changeColor = (event) => {
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+        const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+        $(event.target).css('background', color);
+
+    }
+
+    const numDivs = (event) => {
+        let $box = $('#inputBox')
+        console.log($box.val());
+
+        for (let i = 0; i < $box.val(); i++) {
+            let $div = $('<div>');
+            $div.addClass('square');
+            $('#container').append($div);
+        }
+
+        $('div.square').on('mouseover', changeColor);
+    }
+
+    let $box = $('#inputBox')
+    let $button = $('#inputButton')
+
+    $button.on('click', numDivs)
+    $box.on('keypress', (event) => {
+        if (event.which == 13) {
+            console.log('enter')
+            numDivs()
+        }
+    })
+
+
 });
+
