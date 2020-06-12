8662cf7ba17e13425511a6dd5ea20201eac066b4
Senthil <spk2dc@virginia.edu>
2020-06-12 12:42:52 -0400

    fixed generate error

--- 1_front_end_development/w03d05/student_examples/app.js
+++ 1_front_end_development/w03d05/student_examples/app.js
@@ -5,7 +5,20 @@ $(() => {
     const $myPalette = $('#my-palette');
     const $generate = $('#generate');
 
+    const addColor = (event) => {
+        const color = $(event.currentTarget).css('background-color');
+
+        const $square = $('<div>');
+        $square.addClass('square');
+        $square.css('background-color', color);
+
+        $myPalette.append($square)
+    }
+
     const makePalette = () => {
+        $colorPalette.empty();
+        $myPalette.empty();
+
         for (let i = 0; i < 150; i++) {
             const red = Math.floor(Math.random() * 255);
             const green = Math.floor(Math.random() * 255);
@@ -17,6 +30,8 @@ $(() => {
             $square.addClass('square');
             $colorPalette.append($square);
         }
+
+        $('.square').on('click', addColor);
     }
     $generate.on('click', makePalette);
 
