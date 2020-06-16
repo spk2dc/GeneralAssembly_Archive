7ad5caa9cdd40edfd200085ff30a3cef4244cf9e
Senthil <spk2dc@virginia.edu>
2020-06-16 02:08:01 -0400

    completed morning exercise

--- 1_front_end_development/w04d01/student_labs/complaints_app/app.js
+++ 1_front_end_development/w04d01/student_labs/complaints_app/app.js
@@ -3,8 +3,10 @@ let baseurl = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
 let filterBorough = (event) => {
     let $btn = $(event.currentTarget)
     let num = $('input[type="text"]').val()
-    let filter = `${baseurl}?borough=${$btn.text()}/`
+    let filter = `${baseurl}?borough=${$btn.text()}`
     console.log(filter);
+    console.log(num);
+
 
     $.ajax({
         url: filter,
@@ -13,10 +15,27 @@ let filterBorough = (event) => {
             "$limit": num,
             "$$app_token": "eUIH3YV85vSGtjEUnYoKS3f7r"
         }
-    }).then(function (data) {
-        alert("Retrieved " + data.length + " records from the dataset!");
-        console.log(data);
-    })
+
+    }).then(parseData)
+
+    event.preventDefault()
+    event.stopPropagation()
+
+}
+
+let parseData = (data) => {
+    console.log("Retrieved " + data.length + " records from the dataset!");
+    console.log(data);
+
+    for (const itr of data) {
+        let $tr = $('<tr>')
+        $('table').append($tr)
+        
+        $tr.append($('<td>').text(itr.unique_key))
+        $tr.append($('<td>').text(itr.borough))
+        $tr.append($('<td>').text(itr.descriptor))
+
+    }
 }
 
 $(() => {
