53bf2e045d49f61c6a27aead0e4d939135c5bf9f
Senthil <spk2dc@virginia.edu>
2020-06-24 18:26:40 -0400

    finished bonus for afternoon lab with html user input

--- 2_full_stack_dev/w05d03/student_labs/calculator/server.js
+++ 2_full_stack_dev/w05d03/student_labs/calculator/server.js
@@ -12,26 +12,40 @@ app.get('/calcquery/:num1/:num2', (req, res) => {
     let op = req.query.operation
     let result = 0
 
-    switch (op) {
-        case 'add':
-            result = num1 + num2
-            res.send('sum is: ' + result);
-            break;
-        case 'multiply':
-            result = num1 * num2
-            res.send('multiply is: ' + result);
-            break;
-        case 'subtract':
-            result = num1 - num2
-            res.send('subtract is: ' + result);
-            break;
-        case 'divide':
-            result = num1 / num2
-            res.send('divide is: ' + result);
-            break;
-        default:
-            res.send('no valid operation');
-            break;
+    console.log(op);
+
+    if (typeof op !== 'string') {
+        console.log('get operation');
+
+        res.send(`
+            <form action="/calcquery/${num1}/${num2}?operation=${op}" method="get">
+                <label for="lname">Operation:</label>
+                <input type="text" id="op" name="operation"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        `)
+    } else {
+        switch (op) {
+            case 'add':
+                result = num1 + num2
+                res.send('sum is: ' + result);
+                break;
+            case 'multiply':
+                result = num1 * num2
+                res.send('multiply is: ' + result);
+                break;
+            case 'subtract':
+                result = num1 - num2
+                res.send('subtract is: ' + result);
+                break;
+            case 'divide':
+                result = num1 / num2
+                res.send('divide is: ' + result);
+                break;
+            default:
+                res.send('no valid operation');
+                break;
+        }
     }
 
 });
