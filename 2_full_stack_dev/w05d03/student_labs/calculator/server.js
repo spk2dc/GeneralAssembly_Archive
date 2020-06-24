809cbee49b856de90d1f84b48b1bd1e26434c19f
Senthil <spk2dc@virginia.edu>
2020-06-24 17:47:38 -0400

    finisehd afternoon lab

---
+++ 2_full_stack_dev/w05d03/student_labs/calculator/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('app is running on port: ', port);
+});
+
+app.get('/calcquery/:num1/:num2', (req, res) => {
+    let num1 = parseInt(req.params.num1, 10)
+    let num2 = parseInt(req.params.num2, 10)
+    let op = req.query.operation
+    let result = 0
+
+    switch (op) {
+        case 'add':
+            result = num1 + num2
+            res.send('sum is: ' + result);
+            break;
+        case 'multiply':
+            result = num1 * num2
+            res.send('multiply is: ' + result);
+            break;
+        case 'subtract':
+            result = num1 - num2
+            res.send('subtract is: ' + result);
+            break;
+        case 'divide':
+            result = num1 / num2
+            res.send('divide is: ' + result);
+            break;
+        default:
+            res.send('no valid operation');
+            break;
+    }
+
+});
+
+app.get('/calc/:num1/:num2', (req, res) => {
+    let num1 = parseInt(req.params.num1, 10)
+    let num2 = parseInt(req.params.num2, 10)
+    let sum = num1 + num2
+    res.send('sum is: ' + sum);
+
+})
