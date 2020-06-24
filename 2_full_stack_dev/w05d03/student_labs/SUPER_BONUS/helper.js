92e97ba38980951f20387f9dc5b23b4c3de41b8d
Senthil <spk2dc@virginia.edu>
2020-06-24 13:07:15 -0400

    morning lab bonus part 1 complete

---
+++ 2_full_stack_dev/w05d03/student_labs/SUPER_BONUS/helper.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+// access the data in this file
+const sightingsData = require('./sightings.json')
+
+// test it
+// console.log(sightingsData[0])
+
+// access the argument
+const args = process.argv[2]
+console.log('terminal argument: ', args)
+let param = args.split('=')[0]
+let val = args.split('=')[1]
+
+for (let i = 0; i < sightingsData.length; i++) {
+    if (sightingsData[i][param] === val) {
+        // console.log(sightingsData[i]);
+
+    }
+
+}
+
+app.listen(3000)
+app.get('/ufo', (req, res) => {
+    res.send('req');
+    console.log(req);
+    
+})
