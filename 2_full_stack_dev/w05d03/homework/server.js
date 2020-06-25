9eabf0bad357dbc89ce27ba7862efdef469b366c
Senthil <spk2dc@virginia.edu>
2020-06-24 22:26:45 -0400

    Magic 8 Ball express application created.

--- 2_full_stack_dev/w05d03/homework/server.js
+++ 2_full_stack_dev/w05d03/homework/server.js
@@ -17,3 +17,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     let result = tip / 100 * tot
     res.send(`Tip to pay: $${result}`)
 })
+
+app.get('/magic/:question', (req, res) => {
+    let question = decodeURI(req.params.question)
+    let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+    let oneResp = responses[Math.floor(Math.random()*responses.length)]
+
+    res.send(`Question: ${question}? <br>
+    <h1>${oneResp}</h1>`)
+})
