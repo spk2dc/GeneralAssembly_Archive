81cfeca1135e96c987c97b079f6d302f94aaf204
Senthil <spk2dc@virginia.edu>
2020-06-26 12:44:21 -0400

    finished morning lab

--- 2_full_stack_dev/w05d05/morning_exercise/main.js
+++ 2_full_stack_dev/w05d05/morning_exercise/main.js
@@ -1,5 +1,34 @@
 
 
-let findWordFrequencies = () => {
+let findWordFrequencies = (str) => {
+    let strArr = str.toLowerCase().split(' ')
+    let freq = {}
+
+    for (const i in strArr) {
+        if (freq.hasOwnProperty(strArr[i])) {
+            freq[strArr[i]] += 1
+
+        } else {
+            freq[strArr[i]] = 1
+        }
+    }
     
+    console.log(strArr);
+    console.log(freq);
+    return freq
 }
+
+let findHighestFrequency = (objParam) => {
+    let max = Object.keys(objParam)[0]
+    
+    for (const key in objParam) {
+        if (objParam[key] > objParam[max]) {
+            max = key
+        }
+    }
+    
+    console.log(`${max}: ${objParam[max]}`);
+}
+
+let obj = findWordFrequencies('That that is is that that is not is not')
+findHighestFrequency(obj)
