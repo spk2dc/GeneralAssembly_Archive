cbb15f9f73d654608d478c2a56442c64e33c6d82
Senthil <spk2dc@virginia.edu>
2020-06-25 21:32:54 -0400

    Created show views of each pokemon

--- 2_full_stack_dev/w05d04/homework/pokemon/server.js
+++ 2_full_stack_dev/w05d04/homework/pokemon/server.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
-app.get('/pokemon',(req,res)=>{
-    res.render('index.ejs',{pokemon: pokemon})
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', { pokemon: pokemon })
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { pokemon: pokemon, id: req.params.id })
 })
