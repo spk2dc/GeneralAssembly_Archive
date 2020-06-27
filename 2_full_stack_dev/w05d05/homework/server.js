c100a52140b28cbecd061651c4ab22890bda9b85
Senthil <spk2dc@virginia.edu>
2020-06-26 23:31:06 -0400

    finished index and show pages

--- 2_full_stack_dev/w05d05/homework/server.js
+++ 2_full_stack_dev/w05d05/homework/server.js
@@ -6,24 +6,26 @@ const app = express();
 const port = 3000;
 
 // middleware
-// ?
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+//tells express to try to match requests with files in the directory called 'public'
+app.use(express.static('public'));
 
 // data
 const data = require('./models/budget.js')
 
 // get route
 app.get('/budgets', (req, res) => {
-    res.send(data)
+    // res.send(data)
+    res.render('index.ejs', { data: data })
 })
 
 app.get('/budgets/new', (req, res) => {
-    res.send(data)
+    res.render('new.ejs', { data: data })
 })
 
 app.get('/budgets/:index', (req, res) => {
-    res.send(data)
+    res.render('show.ejs', { data: data, index: req.params.index })
 })
 
 
