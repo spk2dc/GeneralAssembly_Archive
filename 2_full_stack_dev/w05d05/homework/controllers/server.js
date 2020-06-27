5801ae83ad73eefaf891ad7c9b6fc434fe9cb6cc
Senthil <spk2dc@virginia.edu>
2020-06-26 22:31:01 -0400

    setup file structures

---
+++ 2_full_stack_dev/w05d05/homework/controllers/server.js
@@ -0,0 +1,37 @@
+// dependencies
+const express = require('express');
+const app = express();
+
+// port
+const port = 3000;
+
+// middleware
+// ?
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// data
+const dates = [
+  { name: "Bangor", created_at: Date.now() },
+  { name: "San Jose", created_at: Date.now() },
+  { name: "Bozeman", created_at: Date.now() }
+];
+
+// get route
+app.get('/dates', (req, res) => {
+  res.send(dates)
+})
+
+// post route
+app.post('/dates', (req, res) => {
+  console.log('req.body is: ', req.body);
+  if (!req.body.name) {
+    req.body.name = "NO DATA"
+  }
+  req.body.created_at = Date.now()
+  dates.push(req.body)
+  console.log('all the dates data: ', dates)
+  res.redirect('/dates')
+})
+
+app.listen(port, () => console.log('Running on port: ', port));
