8869651c43397bd9be20f84ccaa19f364d5b34be
Senthil <spk2dc@virginia.edu>
2020-06-30 17:57:57 -0400

    just got afternoon lab to print data correctly. needed to update mongoose version

--- 2_full_stack_dev/w06d02/student_labs/afternoon_lab/app.js
+++ 2_full_stack_dev/w06d02/student_labs/afternoon_lab/app.js
@@ -7,21 +7,108 @@ const seedData = require('./models/seed_vampires.js');
 const Vampire = require('./models/vampire.js');
 
 // Configuration
-const mongoURI = 'mongodb://localhost:27017/'+ 'vampires';
+const mongoURI = 'mongodb://localhost:27017/' + 'vampires';
 const db = mongoose.connection;
 
 // Connect to Mongo
-mongoose.connect( mongoURI );
+mongoose.connect(mongoURI);
 
 // Connection Error/Success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', mongoURI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-db.on( 'open' , ()=>{
+db.on('open', () => {
   console.log('Connection made!');
 });
 
 /**********************************************************************
 Write Your Code Below
 **********************************************************************/
+
+var vampire = {
+  name: 'Chocula',
+  title: 'Count',
+  hair_color: 'brown',
+  eye_color: 'brown',
+  dob: new Date(1971, 2, 13, 7, 47),
+  loves: ['cereal', 'marshmallows'],
+  location: 'Minneapolis, Minnesota, US',
+  gender: 'm',
+  victims: 2,
+}
+
+// Vampire.remove()
+
+//to be run once and then commented out
+Vampire.insertMany(seedData, (err, vampires) => {
+  if (err) { console.log(err) }
+  console.log("added provided vampire data", vampires)
+  mongoose.connection.close();
+});
+
+let tempArr = [
+  {
+    name: 'vamp1',
+    title: 'vamptitle1',
+    hair_color: 'black',
+    eye_color: 'black',
+    dob: new Date(2020, 2, 13, 7, 47),
+    loves: ['cookies', 'blood'],
+    location: 'General Assembly',
+    gender: 'm',
+    victims: 13,
+  },
+  {
+    name: 'vamp2',
+    title: 'vamptitle2',
+    hair_color: 'black',
+    eye_color: 'black',
+    dob: new Date(2020, 2, 13, 7, 47),
+    loves: ['cookies', 'blood'],
+    location: 'General Assembly',
+    gender: 'm',
+    victims: 13,
+  },
+  {
+    name: 'vamp3',
+    title: 'vamptitle3',
+    hair_color: 'black',
+    eye_color: 'black',
+    dob: new Date(2020, 2, 13, 7, 47),
+    loves: ['cookies', 'blood'],
+    location: 'General Assembly',
+    gender: 'm',
+    victims: 13,
+  },
+  {
+    name: 'vamp4',
+    title: 'vamptitle4',
+    hair_color: 'black',
+    eye_color: 'black',
+    dob: new Date(2020, 2, 13, 7, 47),
+    loves: ['cookies', 'blood'],
+    location: 'General Assembly',
+    gender: 'm',
+    victims: 13,
+  },
+]
+
+//insert all objects in the array into vampire data
+// Vampire.insertMany(tempArr, (error, data) => {
+//   if (error) { // if there is an error console log it
+//     console.log(error);
+//   } else { // else show us the created data
+//     console.log(data);
+//   }
+//   // get control of terminal back
+//   // you can also just use control-c
+//   db.close();
+// });
+
+//find all data in vampire database
+// Vampire.find((err, data) => {
+//   console.log('error: ', err);
+//   console.log(data);
+//   db.close();
+// });
--- 2_full_stack_dev/w06d02/student_labs/afternoon_lab/models/vampire.js
+++ 2_full_stack_dev/w06d02/student_labs/afternoon_lab/models/vampire.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
   // add your code here to set up your schema
+  name: { type: String, required: true },
+  title: { type: String },
+  hair_color: { type: String, default: 'blonde' },
+  eye_color: String,
+  dob: { type: Date, default: Date.now },
+  loves: [String, String],
+  location: String,
+  gender: String,
+  victims: { type: Number, capped: true, min: 0 },
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
