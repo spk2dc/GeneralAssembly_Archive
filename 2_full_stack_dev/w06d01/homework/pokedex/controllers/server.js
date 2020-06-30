22e6f1a1cc2c0e216946a47a437b77b0fe8cf53a
Senthil <spk2dc@virginia.edu>
2020-06-29 22:56:25 -0400

    edit and update work except for populating edit field with all type array entries

--- 2_full_stack_dev/w06d01/homework/pokedex/controllers/server.js
+++ 2_full_stack_dev/w06d01/homework/pokedex/controllers/server.js
@@ -29,7 +29,8 @@ app.get('/pokemon/:id', (req, res) => {
 
 // EDIT
 app.get('/pokemon/:id/edit', (req, res) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] })
+    // console.log('edit ', Pokemon[req.params.id]);
+    res.render(require.resolve('../views/edit.ejs'), { data: Pokemon[req.params.id], id: req.params.id, allTypes: Pokemon[req.params.id].type.join(', ') })
 })
 
 // CREATE
@@ -55,7 +56,19 @@ app.post('/pokemon', (req, res) => {
 
 // UPDATE
 app.put('/pokemon/:id', (req, res) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] })
+    // console.log('req', req.body);
+
+    Pokemon[req.params.id].name = req.body.name
+    Pokemon[req.params.id].stats.hp = req.body.hp
+    Pokemon[req.params.id].stats.attack = req.body.attack
+    Pokemon[req.params.id].stats.defense = req.body.defense
+
+    let typeStr = req.body.type
+    Pokemon[req.params.id].type = typeStr.split(', ')
+    console.log('updated pokemon ', Pokemon[req.params.id]);
+
+    //only use resolve with file paths, don't use it with relative paths like in redirect links
+    res.redirect('/pokemon')
 })
 
 // DESTROY
