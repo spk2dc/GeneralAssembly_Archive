fd17b589ca0038394f8f04de465190ff3031046b
Ben Manning <pookymane@bens-mbp.lan>
2020-07-01 10:20:42 -0400

    created the new route and view

--- 2_full_stack_dev/w06d03/instructor_examples/fruit_app/server.js
+++ 2_full_stack_dev/w06d03/instructor_examples/fruit_app/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
+// ROUTES
 
+// new
+app.get('/fruits/new', (req, res) => {
+  res.render('new.ejs');
+})
 
 // the app running the server
 app.listen(3000, () => {
