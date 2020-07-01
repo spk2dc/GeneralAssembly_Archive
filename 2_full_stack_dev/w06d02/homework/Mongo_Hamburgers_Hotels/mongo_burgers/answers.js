2d6d92637f432572d0e8cdce738efe7c921021a4
Senthil <spk2dc@virginia.edu>
2020-06-30 21:50:36 -0400

    finished mongo burgers basic homework

--- 2_full_stack_dev/w06d02/homework/Mongo_Hamburgers_Hotels/mongo_burgers/answers.js
+++ 2_full_stack_dev/w06d02/homework/Mongo_Hamburgers_Hotels/mongo_burgers/answers.js
@@ -1,46 +1,93 @@
 // create 5 burgers (at least 3 should be beef)
-
+db.burger.insert([
+    {
+        meat: 'beef',
+        cheese: false,
+        toppings: ['tomatoes', 'onions', 'cucumber']
+    },
+    {
+        meat: 'beef',
+        cheese: false,
+        toppings: ['ketchup', 'tomatoes', 'pickles']
+    },
+    {
+        meat: 'beef',
+        cheese: false,
+        toppings: ['cucumber', 'onions', 'pickles']
+    },
+    {
+        meat: 'chicken',
+        cheese: false,
+        toppings: ['tomatoes', 'onions', 'pickles']
+    },
+    {
+        meat: 'veggie',
+        cheese: false,
+        toppings: ['ketchup', 'cucumber', 'pickles']
+    },
+])
 
 
 // find all the burgers
+db.burger.find()
 
 
 // show just the meat of each burger
+db.burger.find({}, { meat: 1, _id: 0 })
 
 
 // show just the toppings of each burger
+db.burger.find({}, { toppings: 1, _id: 0 })
 
 
 // show everything but the cheese
+db.burger.find({}, { cheese: 0 })
 
 
 // find all the burgers with beef
+db.burger.find({ meat: "beef" })
 
 
 // find all the burgers that are not beef
+db.burger.find({ meat: { $ne: "beef" } })
 
 
 // find the first burger with cheese
+db.burger.findOne({ cheese: true })
 
 
 // find one and update the first burger with cheese to have a property of 'double cheese'
+db.burger.findOneAndUpdate(
+    { cheese: true },
+    {
+        $set: { doubleCheese: true }
+    }
+)
 
 
 // find the burger you updated to have double cheese
-
+db.burger.find({ doubleCheese: true })
 
 // find and update all the beef burgers to be 'veggie'
-
+db.burger.updateMany(
+    { meat: 'beef' },
+    {
+        $set: { meat: 'veggie' }
+    }
+)
 
 // delete one of your veggie burgers
 // WRONG - dELETES ALL : db.burger.remove({meat: 'veggie'})
+db.burger.deleteOne({ meat: 'veggie' })
 
 
 // drop the collection
+db.burger.drop()
 //Expected Output
 //true
 
 // drop the database
+db.dropDatabase()
 //Expected Output
 // {
 //   "dropped": "burgers",
