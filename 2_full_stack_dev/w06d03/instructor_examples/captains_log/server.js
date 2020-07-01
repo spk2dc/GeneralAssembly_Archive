ee01658969787a9c0a75fd9945f3fe26bf39cf9c
jamesgk <james.godfreykittle@generalassemb.ly>
2020-07-01 16:51:45 -0400

    Captain's log: Create and index routes

--- 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
+++ 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
@@ -1,7 +1,48 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Log = require('./models/logs.js');
+
+mongoose.connect('mongodb://localhost:27017/captains_log', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/logs/seed', (req, res) => {
+    Log.remove(() => {
+        Log.create([
+            {
+                title: 'Title 1',
+                entry: 'Log entry 1',
+                shipIsBroken: false,
+            },
+            {
+                title: 'Title 2',
+                entry: 'Log entry 2',
+                shipIsBroken: true,
+            },
+            {
+                title: 'Title 2',
+                entry: 'Log entry 2',
+            },
+        ], (err, data) => {
+            res.redirect('/logs');
+        });
+    })
+});
+
+app.get('/logs', (req, res) => {
+    Log.find((err, allLogs) => {
+        res.render('index.ejs', {
+            logs: allLogs,
+        });
+    });
+});
 
 app.get('/logs/new', (req, res) => {
     res.render('new.ejs');
@@ -13,8 +54,10 @@ app.post('/logs', (req, res) => {
     } else {
         req.body.shipIsBroken = false;
     }
-    res.send(req.body);
-})
+    Log.create(req.body, (err, newLog) => {
+        res.redirect('/logs/' + newLog.id);
+    });
+});
 
 app.listen(3000, () => {
     console.log('listening');
