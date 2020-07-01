e36759f2a6d2d43456450c8bbe4abd5a6e0cc58e
jamesgk <james.godfreykittle@generalassemb.ly>
2020-07-01 16:28:22 -0400

    Captain's log: WIP create route

--- 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
+++ 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/logs/new', (req, res) => {
     res.render('new.ejs');
 });
+
+app.post('/logs', (req, res) => {
+    if (req.body.shipIsBroken === 'on') {
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
+    res.send(req.body);
+})
 
 app.listen(3000, () => {
     console.log('listening');
