c30cfabce1e560d16ac926585ea703ad03f3690a
Senthil <spk2dc@virginia.edu>
2020-07-01 12:02:27 -0400

    create and body parser working

--- 2_full_stack_dev/w06d03/student_labs/captain/server.js
+++ 2_full_stack_dev/w06d03/student_labs/captain/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const db = mongoose.connection
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,3 +14,18 @@ mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true,
 mongoose.connection.once('open', () => {
     console.log('connected to mongo');
 });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//new
+app.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+//create
+app.post('/logs', (req, res) => {
+    res.send(req.body)
+})
