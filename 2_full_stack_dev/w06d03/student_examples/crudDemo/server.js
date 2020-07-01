70ec5fa9b3c2c13206cda2a9e6358949d96a5933
Senthil <spk2dc@virginia.edu>
2020-07-01 11:08:10 -0400

    morning code along not working

---
+++ 2_full_stack_dev/w06d03/student_examples/crudDemo/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Fruit = require('./models/fruits.js');
+
+app.use(express.urlencoded({extended:true}));
+
+app.listen(3000, ()=>{
+    console.log('listening');
+});
+
+app.get('/fruits/new', (req, res)=>{
+    res.render('new.ejs');
+});
+
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.redirect('/fruits');
+    });
+});
+
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('index.ejs', {
+            fruits: allFruits
+        });
+    });
+});
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {
+            fruit:foundFruit
+        });
+    });
+});
