bc258c96f438c80db438e01ef4590478fdedc672
jamesgk <james.godfreykittle@generalassemb.ly>
2020-07-02 14:43:34 -0400

    Captain's log: Delete route

--- 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
+++ 2_full_stack_dev/w06d03/instructor_examples/captains_log/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Log = require('./models/logs.js');
+
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/captains_log', {
     useNewUrlParser: true,
@@ -27,8 +30,8 @@ app.get('/logs/seed', (req, res) => {
                 shipIsBroken: true,
             },
             {
-                title: 'Title 2',
-                entry: 'Log entry 2',
+                title: 'Title 3',
+                entry: 'Log entry 3',
             },
         ], (err, data) => {
             res.redirect('/logs');
@@ -64,6 +67,12 @@ app.post('/logs', (req, res) => {
     }
     Log.create(req.body, (err, newLog) => {
         res.redirect('/logs/' + newLog.id);
+    });
+});
+
+app.delete('/logs/:id', (req, res) => {
+    Log.findByIdAndDelete(req.params.id, (err, deleteLog) => {
+        res.redirect('/logs');
     });
 });
 
