a4806af23e87681318c134640abfb4b665e53d93
Senthil <spk2dc@virginia.edu>
2020-07-15 13:01:08 -0400

    started morning lab using simple react setup instead of installs

---
+++ 3_front_end_frameworks/w08d03/student_labs/dashboard/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Sidebar />
+      </div>
+    )
+  }
+}
+
+export default App
