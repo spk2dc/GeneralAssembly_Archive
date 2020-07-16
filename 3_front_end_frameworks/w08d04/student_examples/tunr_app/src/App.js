727c4846d8b74cab74ec13d0c776168d7fc1e760
Senthil <spk2dc@virginia.edu>
2020-07-16 12:24:27 -0400

    demo forms working

--- 3_front_end_frameworks/w08d04/student_examples/tunr_app/src/App.js
+++ 3_front_end_frameworks/w08d04/student_examples/tunr_app/src/App.js
@@ -1,10 +1,89 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import playlist from './data.js'
+
+console.table(playlist)
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playlist: playlist,
+      title: '',
+      artist: '',
+      time: '0:00'
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+
   render() {
     return (
       <div>
-        hello world
+        <Header />
+
+        <form>
+          <label htmlFor="title">
+            Song
+            <input
+              type="text"
+              id="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="artist">
+            Artist
+            <input
+              type="text"
+              id="artist"
+              value={this.state.artist}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="time">
+            Time
+            <input
+              type="text"
+              id="time"
+              value={this.state.time}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+
+        <main>
+          <div>
+            <h3>Playlist 1</h3>
+            <table>
+              <thead>
+                <tr>
+                  <th>Song</th>
+                  <th>Arist</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.playlist.map((song, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{song.title}</td>
+                      <td>{song.artist}</td>
+                      <td>{song.time}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </main>
+
+        <Footer />
       </div>
     )
   }
