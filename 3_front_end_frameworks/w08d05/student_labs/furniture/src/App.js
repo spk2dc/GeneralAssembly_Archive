653e0efc25e4161f7de2b531ca1f4a7b1441f2bc
Senthil <spk2dc@virginia.edu>
2020-07-17 14:13:13 -0400

    State Added

--- 3_front_end_frameworks/w08d05/student_labs/furniture/src/App.js
+++ 3_front_end_frameworks/w08d05/student_labs/furniture/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class App extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = {
+      chair: "Grandma's Favorite Chair"
+    }
+  }
+
+  render() {
     return (
       <div className='container'>
         <h1 className='shop-name'>Heirloom Furniture Restoration</h1>
