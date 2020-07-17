15895858f2b516f97d266c7cd130327bb816e1f3
Senthil <spk2dc@virginia.edu>
2020-07-17 14:12:30 -0400

    First Component Created

---
+++ 3_front_end_frameworks/w08d05/student_labs/furniture/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <h1 className='shop-name'>Heirloom Furniture Restoration</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
