bb3db4506f212a3af86ae029a5817dcc2ca2a49e
Senthil <spk2dc@virginia.edu>
2020-07-16 21:04:04 -0400

    got toggle and approval working

--- 3_front_end_frameworks/w08d04/student_labs/bird-react/src/App.js
+++ 3_front_end_frameworks/w08d04/student_labs/bird-react/src/App.js
@@ -15,19 +15,47 @@ class App extends React.Component {
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    // this.handleApproval = this.handleApproval.bind(this)
+    this.changeApproval = this.changeApproval.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
   }
 
   handleChange(event) {
+    console.log(`App -> handleApproval -> event.target.value`, event.target.value)
     this.setState({ [event.target.id]: event.target.value })
   }
 
   handleSubmit(event) {
-    this.setState({ [event.target.id]: event.target.value })
+    event.preventDefault()
+    console.log(this.state.approved)
+    const newItem = {
+      birdName: this.state.birdName,
+      image: this.state.image,
+      user: this.state.user,
+      approved: this.state.approved
+    }
+
+    const updatedArr = [...this.state.birdArr, newItem]
+
+    this.setState({
+      birdArr: updatedArr,
+      birdName: '',
+      image: '',
+      user: '',
+      approved: false
+    })
   }
 
-  handleDelete(event) {
-    this.setState({ [event.target.id]: event.target.value })
+  changeApproval(index) {
+    const birdArr = this.state.birdArr
+    birdArr[index].approved = !birdArr[index].approved
+    console.log(`App -> changeApproval -> birdArr`, birdArr[index])
+    this.setState({})
+  }
+
+  handleDelete(index) {
+    let updatedArr = this.state.birdArr.filter((item, i) => i !== index)
+    this.setState({ birdArr: updatedArr })
   }
 
   render() {
@@ -35,7 +63,10 @@ class App extends React.Component {
       <div>
         <h1>Bird Photo Contest</h1>
 
-        {Form(App)}
+        <Form
+          state={this.state}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}/>
 
         <table>
           <thead>
@@ -58,12 +89,13 @@ class App extends React.Component {
                     <td>
                       {
                         item.approved
-                          ? <button onClick={() => this.handleDelete(index)}>
+                          ? <button onClick={() => this.changeApproval(index)}>
                             Unapprove
                             </button>
-                          : <button onClick={() => this.handleDelete(index)}>
+                          : <button onClick={() => this.changeApproval(index)}>
                             Approve
                             </button>
+
                       }
                     </td>
                     <td>
--- 3_front_end_frameworks/w08d04/student_labs/bird-react/src/components/Form.jsx
+++ 3_front_end_frameworks/w08d04/student_labs/bird-react/src/components/Form.jsx
@@ -1,38 +1,56 @@
 import React, { Component } from 'react'
 
-export default function Form(App, handleChange, handleSubmit) {
-    return (
-        <form onSubmit={App.handleSubmit}>
+class Form extends Component {
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit} >
+                <label htmlFor="birdName">
+                    Name
+                  <input
+                        type="text"
+                        id="birdName"
+                        value={this.props.state.birdName}
+                        onChange={this.props.handleChange}
+                    />
+                </label>
+                <br />
+                <label htmlFor="image">
+                    Image
+                  <input
+                        type="text"
+                        id="image"
+                        value={this.props.state.image}
+                        onChange={this.props.handleChange}
+                    />
+                </label>
+                <br />
+                <label htmlFor="user">
+                    User
+                  <input
+                        type="text"
+                        id="user"
+                        value={this.props.state.user}
+                        onChange={this.props.handleChange}
+                    />
+                </label>
+                <br />
+                <label htmlFor="approved">
+                    Approved
+                    <input
+                        type="checkbox"
+                        name="approved"
+                        id="approved"
+                        checked={this.props.state.approved==='on' ? true : false}
+                        onChange={this.props.handleChange}
+                    />
 
-            <label htmlFor="birdName">
-                Name
-              <input
-                    type="text"
-                    id="birdName"
-                    value={App.birdName}
-                    onChange={App.handleChange}
-                />
-            </label>
-            <br />
-            <label htmlFor="image">
-                Image
-              <input
-                    type="text"
-                    id="image"
-                    value={App.image}
-                    onChange={App.handleChange}
-                />
-            </label>
-            <br />
-            <label htmlFor="user">
-                User
-              <input
-                    type="text"
-                    id="user"
-                    value={App.user}
-                    onChange={App.handleChange}
-                />
-            </label>
-        </form>
-    )
+                </label>
+                <br />
+                <input type="submit" />
+            </form>
+        )
+    }
+
 }
+
+export default Form
