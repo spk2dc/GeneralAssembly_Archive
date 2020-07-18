a872c57a8b72dfb267bdfd1b8d43e4fbd039e5be
Senthil <spk2dc@virginia.edu>
2020-07-18 00:23:17 -0400

    apparently this.setstate is asynchronous, switching how score is changed

--- 3_front_end_frameworks/w08d05/homework/jeopardy/src/components/Score.jsx
+++ 3_front_end_frameworks/w08d05/homework/jeopardy/src/components/Score.jsx
@@ -2,31 +2,26 @@ import React from 'react'
 
 export default class Score extends React.Component {
     state = {
-        score: 0,
-        value: 0
+        score: 0
     }
 
     changeScore(event) {
         event.preventDefault()
-        console.log(`Score -> changeScore -> event`, event.target.textContent)
+        // console.log(`Score -> changeScore -> event`, event.target.textContent)
 
         //if there is no question so no property value available, set default value to 0. otherwise set to current property value passed in. needed to prevent errors clicking button when.
+        let tempVal = 0
         isNaN(this.props.value)
-            ? this.setState({
-                value: 0
-            })
-            : this.setState({
-                value: this.props.value
-            })
-        console.log(`Score -> changeScore -> this.props.value`, this.state.value, isNaN(this.props.value), this.props.value)
+            ? tempVal = 0
+            : tempVal = this.props.value
 
         if (event.target.textContent === '+') {
             this.setState({
-                score: this.state.score + this.state.value
+                score: this.state.score + tempVal
             })
         } else {
             this.setState({
-                score: this.state.score - this.state.value
+                score: this.state.score - tempVal
             })
         }
 
