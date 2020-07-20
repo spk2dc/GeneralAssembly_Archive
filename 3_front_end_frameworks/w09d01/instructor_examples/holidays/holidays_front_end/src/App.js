d65db3d293f66e7e1c8e1d06582c17c6dc26b4fd
jamesgk <james.godfreykittle@generalassemb.ly>
2020-07-20 16:04:01 -0400

    MERN lesson: reading data in react

--- 3_front_end_frameworks/w09d01/instructor_examples/holidays/holidays_front_end/src/App.js
+++ 3_front_end_frameworks/w09d01/instructor_examples/holidays/holidays_front_end/src/App.js
@@ -2,21 +2,42 @@ import React from 'react';
 
 let baseUrl = 'http://localhost:3003';
 
-fetch(baseUrl + '/holidays').then(res => {
-  return res.json();
-}).then(data => {
-  console.log(data);
-});
-
 class App extends React.Component {
   state = {
     holidays: [],
+  }
+
+  getHolidays() {
+    fetch(baseUrl + '/holidays').then(res => {
+      return res.json();
+    }).then(data => {
+      this.setState({
+        holidays: data,
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.getHolidays();
   }
 
   render() {
     return (
       <div>
         <h1>Holidays! Celebrate!</h1>
+        <table>
+          <tbody>
+            {
+              this.state.holidays.map(holiday => {
+                return (
+                  <tr key={ holiday._id }>
+                    <td>{ holiday.name }</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
     )
   }
