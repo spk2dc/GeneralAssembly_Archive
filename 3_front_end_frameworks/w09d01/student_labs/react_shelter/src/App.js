90ffca94191337f334fc7f2bb2285a9856004843
Senthil <spk2dc@virginia.edu>
2020-07-20 17:37:57 -0400

    setting up basic react page displaying all animals

--- 3_front_end_frameworks/w09d01/student_labs/react_shelter/src/App.js
+++ 3_front_end_frameworks/w09d01/student_labs/react_shelter/src/App.js
@@ -4,14 +4,42 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      holidays: []
+      animals: []
     }
+  }
+
+  getAnimals() {
+    fetch(baseURL + '/shelter')
+      .then(data => {
+        return data.json()
+      })
+      .then(parsedData => {
+        console.log(parsedData)
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    this.getAnimals()
   }
 
   render() {
     return (
       <div className='container'>
-       <h1>Animal Shelter API</h1>
+        <h1>Animal Shelter API</h1>
+        <table>
+          <tbody>
+            {
+              this.state.animals.map(val => {
+                return (
+                  <tr key={val._id} >
+                    <td> {val.name}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
     )
   }
