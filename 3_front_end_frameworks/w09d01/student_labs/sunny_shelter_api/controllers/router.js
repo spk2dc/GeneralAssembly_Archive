0fdd96055fa7d3b4bfd566e557b4b18bbbffe118
Senthil <spk2dc@virginia.edu>
2020-07-20 13:00:28 -0400

    added second index route to only show ids and names

--- 3_front_end_frameworks/w09d01/student_labs/sunny_shelter_api/controllers/router.js
+++ 3_front_end_frameworks/w09d01/student_labs/sunny_shelter_api/controllers/router.js
@@ -31,4 +31,37 @@ router.get('/', (req, res) => {
     })
 })
 
+// index route that only shows ids and names
+//curl http://localhost:3003/shelter/showids
+router.get('/showids', (req, res) => {
+    Animal.find({}, (err, foundAnimal) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+
+        let arr = foundAnimal.map((val, i) => {
+            let obj = {
+                _id: val._id,
+                name: val.name
+            }
+            return obj
+        })
+        res.status(200).json(arr)
+    })
+})
+
+/**************************************************************
+************************* DELETE ROUTE ************************
+
+curl curl -X DELETE http://localhost:3003/shelter/5f15c9728f578a27cd4dedad
+**************************************************************/
+router.delete('/:id', (req, res) => {
+    Animal.findByIdAndRemove(req.params.id, (err, deletedAnimal) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        res.status(200).json(deletedAnimal)
+    })
+})
+
 module.exports = router
