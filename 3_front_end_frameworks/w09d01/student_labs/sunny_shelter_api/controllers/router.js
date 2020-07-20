efe3d1df728b927f1026475e9231b8058061d6d9
Senthil <spk2dc@virginia.edu>
2020-07-20 12:45:29 -0400

    adding api index route

--- 3_front_end_frameworks/w09d01/student_labs/sunny_shelter_api/controllers/router.js
+++ 3_front_end_frameworks/w09d01/student_labs/sunny_shelter_api/controllers/router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Animal = require('../models/animal.js')
 
 router.get('/', (req, res) => {
-  res.send('index')
+    res.send('index')
 })
 
 /**************************************************************
@@ -13,12 +13,26 @@ router.get('/', (req, res) => {
 curl -X POST -H "Content-Type: application/json" -d '{"name":"test"}' http://localhost:3003/shelter
 **************************************************************/
 router.post('/', async (req, res) => {
-  Animal.create(req.body, (error, createdAnimal) => {
-    if (error) {
-      res.status(400).json({ error: error.message })
-    }
-    res.status(200).send(createdAnimal) //  .json() will send proper headers in response so client knows it's json coming back
-  })
+    Animal.create(req.body, (error, createdAnimal) => {
+        if (error) {
+            res.status(400).json({ error: error.message })
+        }
+        res.status(200).send(createdAnimal) //  .json() will send proper headers in response so client knows it's json coming back
+    })
+})
+
+/**************************************************************
+************************* INDEX ROUTE ************************
+
+curl http://localhost:3003/shelter
+**************************************************************/
+router.get('/', (req, res) => {
+    Holiday.find({}, (err, foundAnimal) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        res.status(200).json(foundAnimal)
+    })
 })
 
 module.exports = router
