74ad857c23033c02f1e42b05feceedd46a4b53da
John Jacobs <john.a.jacobs1973@gmail.com>
2020-07-22 17:54:33 -0400

    completed router

--- 3_front_end_frameworks/w09d02/instructor_examples/router_example/src/App.js
+++ 3_front_end_frameworks/w09d02/instructor_examples/router_example/src/App.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Shop from './components/Shop'
+import NavBar from './components/NavBar'
+import Error from './components/Error'
 
 export default class App extends Component {
+  state = {
+    message: 'Hello',
+    redirect: false
+  }
+
+  handleRedirect = () => {
+    this.setState({
+      redirect: !this.state.redirect
+    })
+  }
+
+  componentDidUpdate() {
+    if (this.state.redirect) {
+      this.setState({
+        redirect: false
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route
+            exact path='/shop'
+            render={() => <Shop message={this.state.message}
+              redirect={this.state.redirect}
+              handleRedirect={this.handleRedirect}
+            />}
+          />
+          <Route component={Error} />
+        </Switch>
       </div>
     )
   }
--- 3_front_end_frameworks/w09d02/instructor_examples/router_example/src/components/Shop.jsx
+++ 3_front_end_frameworks/w09d02/instructor_examples/router_example/src/components/Shop.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-export default function Shop() {
+export default function Shop(props) {
+    if (props.redirect) {
+        return <Redirect to='/'/>
+    }
     return (
         <div>
+            <h1>This is the Shop!</h1>
+            <h2>{props.message}</h2>
+            <button onClick={() => props.handleRedirect()}>CLICK ME</button>
         </div>
     )
 }
