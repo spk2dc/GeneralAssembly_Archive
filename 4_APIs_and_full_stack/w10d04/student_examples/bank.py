9f45d79c7c80b01b0823e08df5fe9e23a8719e1b
Senthil <spk2dc@virginia.edu>
2020-07-30 14:35:49 -0400

    finished lab

--- 4_APIs_and_full_stack/w10d04/student_examples/bank.py
+++ 4_APIs_and_full_stack/w10d04/student_examples/bank.py
@@ -4,9 +4,26 @@ class Bank:
         self.accountType = accountType
         self.balance = balance
         Bank.allAccountsVal += self.balance
+        print(f"Account {self.accountType} created with {self.balance} balance")
     
     def deposit(self, num):
         self.balance += num
+        Bank.allAccountsVal += num
+        print(f'New balance: {myBank.balance}')
 
     def withdraw(self, num):
         self.balance -= num
+        Bank.allAccountsVal -= num
+        print(f'New balance: {myBank.balance}')
+    
+    def printAllAccountsVal(self):
+        print(f'Total balance: {Bank.allAccountsVal}')
+
+
+myBank = Bank()
+myBank.deposit(10)
+myBank.printAllAccountsVal()
+
+myBank2 = Bank('savings',200)
+myBank2.withdraw(50)
+myBank.printAllAccountsVal()
