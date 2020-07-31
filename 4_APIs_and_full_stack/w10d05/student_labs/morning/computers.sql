9bb6467f8720eb2a20c3e935cb67f98b12adf7a3
Senthil <spk2dc@virginia.edu>
2020-07-31 13:05:05 -0400

    finished lab

--- 4_APIs_and_full_stack/w10d05/student_labs/morning/computers.sql
+++ 4_APIs_and_full_stack/w10d05/student_labs/morning/computers.sql
@@ -1,19 +1,29 @@
 -- Write SQL commands under the prompts below, and run the file to get results.
 
 -- In case there is already a computers table, drop it
-
+drop table computers;
 
 -- Create a computers table
+CREATE TABLE computers ( id integer primary key autoincrement not null, make varchar(20), model varchar(20), cpu_speed integer, memory_size integer);
 
 -- The table should have id, make, model, cpu_speed, memory_size,
 --  price, release_date, photo_url, storage_amount, number_usb_ports,
 --  number_firewire_ports, number_thunderbolt_ports
 
+pragma table_info(computers);
 
 -- Insert 4 computers into the computers table
+INSERT INTO computers ( make, model, cpu_speed, memory_size ) VALUES ( 'lenovo', '14inch', 2, 250);
+INSERT INTO computers ( make, model, cpu_speed, memory_size ) VALUES ( 'toshiba', '14inch', 1, 250);
+INSERT INTO computers ( make, model, cpu_speed, memory_size ) VALUES ( 'dell', '14inch', 3, 250);
+INSERT INTO computers ( make, model, cpu_speed, memory_size ) VALUES ( 'microsoft', '14inch', 4, 250);
+
 
 
 -- Select all entries from the computers table
+.headers ON
+.mode columns
+select * from computers;
 
 
 -- HUNGRY FOR MORE?
--- 4_APIs_and_full_stack/w10d05/student_labs/morning/televisions.sql
+++ 4_APIs_and_full_stack/w10d05/student_labs/morning/televisions.sql
@@ -1,19 +1,27 @@
 -- Write SQL commands under the prompts below, and run the file to get results.
 
 -- In case there is already a televisions table, drop it
-
+drop table television;
 
 -- Create a televisions table
+CREATE TABLE television ( id integer primary key autoincrement not null, model_name varchar(20), screen_size integer, price integer, release_date varchar(20));
+
+pragma table_info(television);
 
 --  The table should have id, model_name, screen_size,
 --  price, release_date, photo_url
 
 
 -- Insert 4 televisions into the tv_models table
-
+INSERT INTO television ( model_name, screen_size, price, release_date ) VALUES ( 'hd', 20, 2, 'July 2020');
+INSERT INTO television ( model_name, screen_size, price, release_date ) VALUES ( 'not hd', 30, 1, 'May 2003');
+INSERT INTO television ( model_name, screen_size, price, release_date ) VALUES ( 'cathode', 40, 3, 'Sep 2040');
+INSERT INTO television ( model_name, screen_size, price, release_date ) VALUES ( 'broken', 50, 4, 'Dec 2018');
 
 -- Select all entries from the tv_models table
-
+.headers ON
+.mode columns
+select * from television;
 
 -- HUNGRY FOR MORE? 
 -- Alter the tv_models and add vertical_resolution and horizontal_resolution columns
