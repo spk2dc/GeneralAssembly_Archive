582cf0a843704eab3bf7fcbf567ce3ec77eb4239
Senthil <spk2dc@virginia.edu>
2020-07-31 18:36:04 -0400

    close to figuring out clue 4

--- 4_APIs_and_full_stack/w10d05/homework/carmen-sandiego/find_carmen.sql
+++ 4_APIs_and_full_stack/w10d05/homework/carmen-sandiego/find_carmen.sql
@@ -71,7 +71,17 @@ SELECT name, language, percentage
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+.print ' '
+.print ' Clue #4 '
+.width 0 0 0
 
+SELECT country.name as 'Country Name', city.name as City, district
+    FROM country
+    INNER JOIN countrylanguage
+    ON country.code = countrylanguage.countrycode
+    INNER JOIN city
+    ON country.code = city.countrycode
+    WHERE region = 'Southern Europe' AND language = 'Italian' AND percentage = 100;
 
 
 
