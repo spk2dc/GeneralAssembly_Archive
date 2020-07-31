799dc631afe96586b4fb8bcbaace6eff5538a4ba
John Jacobs <john.a.jacobs1973@gmail.com>
2020-07-31 09:58:36 -0400

    morning lecture added

---
+++ 4_APIs_and_full_stack/w10d05/instructor_examples/companies.sql
@@ -0,0 +1,3 @@
+INSERT INTO companies (name) VALUES ('General Assembly');
+INSERT INTO companies (name) VALUES ('Google');
+INSERT INTO companies (name) VALUES ('Invesco');
---
+++ 4_APIs_and_full_stack/w10d05/instructor_examples/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users ( name, age, email ) VALUES ( 'Matt', 48, 'matthew@generalassemb.ly');
+INSERT INTO users ( name, age, email ) VALUES ( 'Mattias', 48, 'mattias@generalassemb.ly');
+INSERT INTO users ( name, age, email ) VALUES ( 'John', 47, 'john.jacobs@generalassemb.ly');
+INSERT INTO users ( name, age, email ) VALUES ( 'John', 19, 'jonathan@generalassemb.ly');
+INSERT INTO users ( name, age, email ) VALUES ( 'Deja', 23, 'deja@generalassemb.ly');
+INSERT INTO users ( name, age, email ) VALUES ( 'Ben', 36, 'ben@generalassemb.ly');
+INSERT INTO users ( name, email ) VALUES ( 'Brad','b-rad@generalassemb.ly');
