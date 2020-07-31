4e9ebc77aac394eb4316452c166f8825cd5e3800
Senthil <spk2dc@virginia.edu>
2020-07-31 17:54:09 -0400

    adding print statements and starting 2nd clue

--- 4_APIs_and_full_stack/w10d05/homework/carmen-sandiego/find_carmen.sql
+++ 4_APIs_and_full_stack/w10d05/homework/carmen-sandiego/find_carmen.sql
@@ -33,7 +33,14 @@ SELECT MIN(population), name
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+.print ' '
+.print 'All Tables: '
+.tables
+.print ' '
 
+SELECT MIN(population), name 
+    FROM country
+    WHERE region = 'Southern Europe';
 
 
 
