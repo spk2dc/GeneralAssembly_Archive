a9d0807d593213ff7ceb2ced5b78ac1ef1af186a
Senthil <spk2dc@virginia.edu>
2020-07-31 16:29:17 -0400

    got second command to work in lab

--- 4_APIs_and_full_stack/w10d05/student_labs/afternoon/lab.sql
+++ 4_APIs_and_full_stack/w10d05/student_labs/afternoon/lab.sql
@@ -1,7 +1,22 @@
 .headers ON
 .mode columns
 
+
+-- Find all airports that originate from New York
+
 /* note that once you rename a table, you MUST refer to it by its new name */
 SELECT * FROM airports AS origin WHERE origin.city = 'New York';
 /* later on in the SQL statement, when dealing with the destination, you should do the same for airports AS destination */
+
+
+-- Find all destination airports in Paris
+select * from airports as destination where destination.city = 'Paris';
+
+-- Find out how many routes originate from New York
+
+
+-- Find out how many routes have destinations in Paris
+
+
+-- Try to decide which statements are necessary and find how to combine them to find out how many routes originate from New York and land in Paris!
 
