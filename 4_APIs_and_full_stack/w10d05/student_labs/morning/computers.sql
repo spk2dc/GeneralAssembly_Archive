a48ff8feda820bf0c103743f2aceecbbaf97ef9c
Ben Manning <pookymane@Bens-MacBook-Pro.local>
2020-07-31 11:14:10 -0400

    add the lab and the link to the lab

---
+++ 4_APIs_and_full_stack/w10d05/student_labs/morning/computers.sql
@@ -0,0 +1,20 @@
+-- Write SQL commands under the prompts below, and run the file to get results.
+
+-- In case there is already a computers table, drop it
+
+
+-- Create a computers table
+
+-- The table should have id, make, model, cpu_speed, memory_size,
+--  price, release_date, photo_url, storage_amount, number_usb_ports,
+--  number_firewire_ports, number_thunderbolt_ports
+
+
+-- Insert 4 computers into the computers table
+
+
+-- Select all entries from the computers table
+
+
+-- HUNGRY FOR MORE?
+-- Alter the computers_models, adding a storage_type column
