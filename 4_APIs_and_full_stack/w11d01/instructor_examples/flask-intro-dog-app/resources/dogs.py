c840ed993a9b72ba05a739508c05af759f82813b
jamesgk <james.godfreykittle@generalassemb.ly>
2020-08-03 13:07:07 -0400

    Flask get route with data

--- 4_APIs_and_full_stack/w11d01/instructor_examples/flask-intro-dog-app/resources/dogs.py
+++ 4_APIs_and_full_stack/w11d01/instructor_examples/flask-intro-dog-app/resources/dogs.py
@@ -5,3 +5,12 @@ from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict
 
 dog = Blueprint('dogs', 'dog')
+
+@dog.route('/', methods=["GET"])
+def get_all_dogs():
+    try:
+        dogs = [model_to_dict(dog) for dog in models.Dog.select()]
+        # print(dogs)
+        return jsonify(data=dogs, status={'code': 200, 'message': 'Success'})
+    except:
+        return jsonify(data={}, status={'code': 500, 'message': 'Error getting resources'})
---
+++ 4_APIs_and_full_stack/w11d01/instructor_examples/flask-intro-dog-app/seed.sql
@@ -0,0 +1,2 @@
+DELETE FROM dog;
+INSERT INTO dog (name, owner, breed, created_at) VALUES ('Fido', 'James', 'Mutt', datetime('now'));
