db3b2e290fa4508fc3df4c07cfd388b2c95892e7
Senthil <spk2dc@virginia.edu>
2020-08-04 17:27:53 -0400

    adding username field and registration method

--- 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Register.jsx
+++ 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Register.jsx
@@ -3,26 +3,73 @@ import React, { useState } from "react";
 export default function Register() {
   //State Hook
   const [inputs, setInputs] = useState({
+    username: "",
     email: "",
     password: "",
   });
 
   const handleChange = (event) => {
+    let username =
+      "username" === event.target.name ? event.target.value : inputs.username;
     let email =
       "email" === event.target.name ? event.target.value : inputs.email;
     let password =
       "password" === event.target.name ? event.target.value : inputs.password;
 
     setInputs({
+      username: username,
       email: email,
       password: password,
+    });
+  };
+
+  const register = (event) => {
+    event.preventDefault();
+
+    let requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: inputs.username,
+        email: inputs.email,
+        password: inputs.password,
+      }),
+    };
+    // console.log(`register -> inpObj`, inpObj);
+
+    fetch("http://localhost:8000/user/register/", requestOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(`register -> data`, data);
+      })
+      .catch((err) => {
+        console.log(`register -> err`, err);
+      });
+
+    setInputs({
+      username: "",
+      email: "",
+      password: "",
     });
   };
 
   return (
     <div>
       <h1>Register</h1>
-      <form onSubmit='' method='POST'>
+      <form onSubmit={(e) => register(e)} method='POST'>
+        <label htmlFor='username'>Username</label>
+        <input
+          type='text'
+          id='username'
+          name='username'
+          value={inputs.username}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
         <label htmlFor='email'>Email</label>
         <input
           type='text'
