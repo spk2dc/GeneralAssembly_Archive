d0dc259b515235ed0b3c4dfa49da59e1702dd555
Senthil <spk2dc@virginia.edu>
2020-08-04 17:04:54 -0400

    adding login and register fields

--- 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Register.jsx
+++ 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Register.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Register() {
+  //State Hook
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    let email =
+      "email" === event.target.name ? event.target.value : inputs.email;
+    let password =
+      "password" === event.target.name ? event.target.value : inputs.password;
+
+    setInputs({
+      email: email,
+      password: password,
+    });
+  };
+
   return (
     <div>
-      <h1>Oops! No Page Here!</h1>
+      <h1>Register</h1>
+      <form onSubmit='' method='POST'>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          id='email'
+          name='email'
+          value={inputs.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='text'
+          id='password'
+          name='password'
+          value={inputs.password}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <button type='submit'>Sign Up</button>
+      </form>
     </div>
   );
 }
