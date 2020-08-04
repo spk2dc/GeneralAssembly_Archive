f6029156872f83c28370167d5f8bae01abf78096
Senthil <spk2dc@virginia.edu>
2020-08-04 01:30:07 -0400

    made form states change instead of replace previous values

--- 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Form.jsx
+++ 4_APIs_and_full_stack/w11d01/homework/react-dogs/src/components/Form.jsx
@@ -2,35 +2,82 @@ import React, { useState } from "react";
 
 const Form = () => {
   //State Hook
-  const [inputs, setInputs] = useState([]);
+  const [inputs, setInputs] = useState({
+    name: "",
+    owner: "",
+    breed: "",
+  });
 
-  const createDogs = async () => {
+  const createDogs = () => {
     let requestOptions = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name: "Snoopy", owner: "Ben", breed: "Beagle" }),
+      body: JSON.stringify({
+        name: inputs.name,
+        owner: inputs.owner,
+        breed: inputs.breed,
+      }),
     };
 
-    const response = await fetch(
-      "http://localhost:8000/api/v1/dogs/",
-      requestOptions
-    );
-    const promise = await response.json();
-    console.log(`createDogs -> promise`, promise);
+    fetch("http://localhost:8000/api/v1/dogs/", requestOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(`createDogs -> data`, data);
+      })
+      .catch((err) => {
+        console.log(`createDogs -> err`, err);
+      });
+
+    setInputs({
+      name: "",
+      owner: "",
+      breed: "",
+    });
+  };
+
+  const handleChange = (event) => {
+    let name = "name" == event.target.name ? event.target.value : inputs.name;
+    let owner =
+      "owner" == event.target.name ? event.target.value : inputs.owner;
+    let breed =
+      "breed" == event.target.name ? event.target.value : inputs.breed;
+
+    setInputs({
+      name: name,
+      owner: owner,
+      breed: breed,
+    });
   };
 
   return (
-    <form action={createDogs()} method='POST'>
+    <form onSubmit={""} method='POST'>
       <label htmlFor='name'>Name</label>
-      <input type='text' id='name' name='name' />
+      <input
+        type='text'
+        id='name'
+        name='name'
+        onChange={(e) => handleChange(e)}
+      />
       <br />
       <label htmlFor='owner'>Owner</label>
-      <input type='text' id='owner' name='owner' />
+      <input
+        type='text'
+        id='owner'
+        name='owner'
+        onChange={(e) => handleChange(e)}
+      />
       <br />
       <label htmlFor='breed'>Breed</label>
-      <input type='text' id='breed' name='breed' />
+      <input
+        type='text'
+        id='breed'
+        name='breed'
+        onChange={(e) => handleChange(e)}
+      />
       <br />
       <button type='submit'>Submit</button>
     </form>
