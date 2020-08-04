31e9bd49c4db4aa54429adbcd6ad32caad3e7e18
jamesgk <james.godfreykittle@generalassemb.ly>
2020-08-04 16:03:56 -0400

    Flask auth in-class code

--- 4_APIs_and_full_stack/w11d02/instructor_examples/flask-intro-dog-app/app.py
+++ 4_APIs_and_full_stack/w11d02/instructor_examples/flask-intro-dog-app/app.py
@@ -1,13 +1,28 @@
 from flask import Flask, jsonify, g
 from flask_cors import CORS
+from flask_login import LoginManager
 
 import models
 from resources.dogs import dog
+from resources.user import user
 
 DEBUG = True
 PORT = 8000
 
+login_manager = LoginManager()
+
 app = Flask(__name__)
+
+# Set some settings for our user authentication
+app.secret_key = "LJAKLJLKJJLJKLSDJLKJASD"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+  try:
+    return models.User.get(models.User.id == userid)
+  except models.DoesNotExist:
+    return None
 
 # logic for the DB
 @app.before_request
@@ -24,8 +39,10 @@ def after_request(response):
     return response
 
 CORS(dog, origins=['http://localhost:3000'], supports_credentials=True)
-
 app.register_blueprint(dog, url_prefix='/api/v1/dogs')
+
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
+app.register_blueprint(user, url_prefix='/user')
 
 # routes that we have create
 @app.route('/')
